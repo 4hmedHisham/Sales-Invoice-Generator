7bcf348d27ea9a51bea03ae78d04d28dd397872f
Ahmed Hisham <43798851+4hmedHisham@users.noreply.github.com>
2022-02-20 20:18:36 +0200

    Add files via upload

---
+++ Sales_Invoice_Generator/src/DataStoreIO.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class DataStoreIO {
+private String CurrentLine;
+String filepath;
+public String fulldata;
+FileInputStream fis;
+DataStoreIO(String inputpath) throws IOException {
+     filepath=inputpath;
+    fis = new FileInputStream(filepath);
+    int size = fis.available();
+    byte [] b = new byte[size];
+    fis.read(b);
+     fulldata=new String(b);
+
+    //System.out.println(fulldata);
+
+}
+
+
+}
---
+++ Sales_Invoice_Generator/src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import javax.xml.crypto.Data;
+import java.awt.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args []) throws IOException {
+
+    GUI MainFrame= new GUI();
+        //DataStoreIO ionew=new DataStoreIO();
+
+
+    }
+}
