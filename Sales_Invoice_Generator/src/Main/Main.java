299f105e8c6c9a4ce3c975effd68d71eae54e278
Gouda <4hmed.hisham@gmail.com>
2022-03-22 21:21:40 +0200

    Added Packages and its relevant Classes

---
+++ Sales_Invoice_Generator/src/Main/Main.java
@@ -0,0 +1,26 @@
+package Main;
+
+import GUI.GUI;
+
+import javax.swing.*;
+import javax.xml.crypto.Data;
+import java.awt.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args []) throws IOException {
+
+    GUI MainFrame= new GUI();
+//        SimpleDateFormat formatter2=new SimpleDateFormat("dd-MM-yyyy");
+//        try {
+//            Date today= formatter2.parse("22-11-2022");
+//            System.out.println("DATE IS "+today);
+//        }
+//        catch (Exception e ){
+//            System.out.println("PRASING ERROR");
+//        }
+        //FileOperations.DataStoreIO ionew=new FileOperations.DataStoreIO();
+
+
+    }
+}
